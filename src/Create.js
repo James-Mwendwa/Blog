e0fb635b467e8361df58494f5f8e7658a0cc3450
James Mwendwa <mwendwajames32@gmail.com>
2022-09-21 23:04:08 +0300

    functionalities

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Create.css";
 
 function Create() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitle(title);
+    alert(`form submitted ${title}`);
+  };
+
   return (
     <div className="create">
       <div className="createTitle">
-        <form>
-          <input type="text" placeholder="Article title.." />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Article title.."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
           <div className="underline"></div>
-          <textarea placeholder="Tell your story.." />
+          <textarea
+            placeholder="Tell your story.."
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
           <button>Publish</button>
         </form>
       </div>
--- src/Home.js
+++ src/Home.js
@@ -1,47 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 
 function Home() {
+  const [blogs, setBlogs] = useState([]);
+  //eslint-disable-next-line
+  const fetchBlogs = async () => {
+    const res = await fetch(" http://localhost:8000/blogs");
+    const json = await res.json();
+    setBlogs(json);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, [fetchBlogs]);
   return (
     <div className="home">
-      <div className="homeCard">
-        <h2>Title</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolorem
-          dolorum, itaque saepe enim repudiandae ducimus veniam velit delectus
-          magnam quisquam sequi voluptatem eum quos eveniet. Sit repellat
-          doloremque quis. Perferendis hic officiis maiores dolorem? Nesciunt
-          quia non, cumque repellendus in unde praesentium? Distinctio, odit
-          cumque! Officiis autem molestiae ipsam modi, quasi iure nemo? Soluta
-          velit fuga accusamus eligendi tenetur.
-        </p>
-      </div>
-
-      <div className="homeCard">
-        <h2>Title</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolorem
-          dolorum, itaque saepe enim repudiandae ducimus veniam velit delectus
-          magnam quisquam sequi voluptatem eum quos eveniet. Sit repellat
-          doloremque quis. Perferendis hic officiis maiores dolorem? Nesciunt
-          quia non, cumque repellendus in unde praesentium? Distinctio, odit
-          cumque! Officiis autem molestiae ipsam modi, quasi iure nemo? Soluta
-          velit fuga accusamus eligendi tenetur.
-        </p>
-      </div>
-
-      <div className="homeCard">
-        <h2>Title</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolorem
-          dolorum, itaque saepe enim repudiandae ducimus veniam velit delectus
-          magnam quisquam sequi voluptatem eum quos eveniet. Sit repellat
-          doloremque quis. Perferendis hic officiis maiores dolorem? Nesciunt
-          quia non, cumque repellendus in unde praesentium? Distinctio, odit
-          cumque! Officiis autem molestiae ipsam modi, quasi iure nemo? Soluta
-          velit fuga accusamus eligendi tenetur.
-        </p>
-      </div>
+      {blogs.map((blog) => {
+        return (
+          <div className="homeCard" key={blog.id}>
+            <h1>{blog.author}</h1>
+            <h2>{blog.title}</h2>
+            <p>{blog.body}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
